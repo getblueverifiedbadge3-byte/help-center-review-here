211c8340142caecaf8c64207b0d901d9cafa976a
getblueverifiedbadge3-byte <get.blue.verified.badge3@gmail.com>
2025-09-26 20:19:44 +0500

    Delete static directory

--- static/main.ts
+++
@@ -1,4 +0,0 @@
-import { serve } from "https://deno.land/std@0.202.0/http/server.ts";
-import { serveDir } from "https://deno.land/std@0.202.0/http/file_server.ts";
-
-serve((req) => serveDir(req, { fsRoot: ".", showDirListing: false }));
