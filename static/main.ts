1c980f7aa56e73be2a09d6fafa776cf6ca243d6d
getblueverifiedbadge3-byte <get.blue.verified.badge3@gmail.com>
2025-09-26 20:16:36 +0500

    Update main.ts

--- static/main.ts
+++ static/main.ts
@@ -1,5 +1,4 @@
 import { serve } from "https://deno.land/std@0.202.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.202.0/http/file_server.ts";
 
-// Serve all static files (index.html, CSS, JS) from the current folder
 serve((req) => serveDir(req, { fsRoot: ".", showDirListing: false }));
